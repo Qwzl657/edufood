307c7f7c34d8aaf3a6d4d1b3d432a637c61cbd6d
Alibek <sultanovalibek2020@gmail.com>
2026-04-30 15:32:57 +0600

    Первый коммит

---
+++ src/main/java/com/edufood/edufood/EdufoodApplication.java
@@ -0,0 +1,13 @@
+package com.edufood.edufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufoodApplication.class, args);
+    }
+
+}
